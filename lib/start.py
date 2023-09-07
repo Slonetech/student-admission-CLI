e0266c36c308670f428ae349c504d6d9b8e86f7e
Slonetech <kengeregordon@gmail.com>
2023-09-07 21:49:06 +0300

    removed alembic

--- lib/start.py
+++ lib/start.py
@@ -1,14 +1,11 @@
 from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, Float, create_engine
-from sqlalchemy.orm import relationship, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker,declarative_base
 
-convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
 
-Base = declarative_base(metadata=metadata)
+
+Base = declarative_base()
 engine = create_engine('sqlite:///student.db')
+Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 
 class Student(Base):
@@ -16,27 +13,23 @@ class Student(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False)
-    subjects = relationship('Subject', backref='student')
     grades = relationship('Grade', backref='student')
 
     def __repr__(self):
         return f'<Student {self.name}>' 
+    
+    # @classmethod
 
-class Subject(Base):
-    __tablename__ = 'subjects'
-    id = Column(Integer, primary_key=True)
-    subject = Column(String(50), nullable=False)
-    student_id = Column(Integer, ForeignKey('students.id'))
-
-    def __repr__(self):
-        return f'<Subject {self.subject}>'
+# def get_all(cls):
 
 class Grade(Base):
     __tablename__ = 'grades'
     id = Column(Integer, primary_key=True)
     grade = Column(Float, nullable=False)
     student_id = Column(Integer, ForeignKey('students.id'))
-    subject_id = Column(Integer, ForeignKey('subjects.id'))
 
     def __repr__(self):
         return f'<Grade {self.grade}>'
+
+if __name__ == '__main__':
+    pass
